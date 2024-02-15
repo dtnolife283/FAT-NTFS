131cd8db65f27ed632ceea953f57d05dabab7c05
dtnolife283 <tdtuan22@clc.fitus.edu.vn>
2024-02-15 14:15:02 +0700

    Fixed into FAT32

--- BootSector.cpp
+++ BootSector.cpp
@@ -1,6 +1,6 @@
 #include "BootSector.h"
 
-//Open disk
+// Open disk
 BootSector::BootSector() {
     hDevice = CreateFileW(L"\\\\.\\D:", GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
     if (hDevice == INVALID_HANDLE_VALUE) {
@@ -9,12 +9,12 @@ BootSector::BootSector() {
     }
 }
 
-//Close disk
+// Close disk
 BootSector::~BootSector() {
     CloseHandle(hDevice);
 }
 
-//Read boot sector
+// Read boot sector
 void BootSector::ReadBootSector() {
     bResult = ReadFile(hDevice, bBootSector, sizeof(bBootSector), &dwBytesRead, NULL);
     if (!bResult || dwBytesRead != sizeof(bBootSector)) {
@@ -36,14 +36,11 @@ void BootSector::ReadBootSector() {
     NumberOfFATsInt = bBootSector[16];
 
     memcpy(RootEntries, bBootSector + 17, 2);
-    RootEntriesInt = (RootEntries[1] << 8) + RootEntries[0];
 
     memcpy(TotalSectors16, bBootSector + 19, 2);
-    TotalSectors16Int = (TotalSectors16[1] << 8) + TotalSectors16[0];
-
     MediaDescriptor = bBootSector[21];
-    memcpy(SectorsPerFAT, bBootSector + 22, 2);
-    SectorsPerFATInt = (SectorsPerFAT[1] << 8) + SectorsPerFAT[0];
+
+    memcpy(SectorsPerFAT16, bBootSector + 22, 2);
 
     memcpy(SectorsPerTrack, bBootSector + 24, 2);
     SectorsPerTrackInt = (SectorsPerTrack[1] << 8) + SectorsPerTrack[0];
@@ -51,36 +48,53 @@ void BootSector::ReadBootSector() {
     memcpy(NumberOfHeads, bBootSector + 26, 2);
     NumberOfHeadsInt = (NumberOfHeads[1] << 8) + NumberOfHeads[0];
 
-    memcpy(HiddenSectors, bBootSector + 28, 4);
-    HiddenSectorsInt = (HiddenSectors[3] << 24) + (HiddenSectors[2] << 16) + (HiddenSectors[1] << 8) + HiddenSectors[0];
+    memcpy(Distance, bBootSector + 28, 4);
 
     memcpy(TotalSectors32, bBootSector + 32, 4);
-    TotalSectors32Int = (TotalSectors32[3] << 24) + (TotalSectors32[2] << 16) + (TotalSectors32[1] << 8) + TotalSectors32[0];
+    TotalSectorsInt = (TotalSectors32[3] << 24) + (TotalSectors32[2] << 16) + (TotalSectors32[1] << 8) + TotalSectors32[0];
+    
+    memcpy(SectorsPerFAT32, bBootSector + 36, 4);
+    SectorsPerFATInt = (SectorsPerFAT32[3] << 24) + (SectorsPerFAT32[2] << 16) + (SectorsPerFAT32[1] << 8) + SectorsPerFAT32[0];
 
-    PhysicalDriveNumber = bBootSector[36];
-    PhysicalDriveNumberInt = (int)bBootSector[36];
+    memcpy(ExtFlag, bBootSector + 40, 2);
+    memcpy(FSVersion, bBootSector + 42, 2);
 
-    Reserved = bBootSector[37];
-    ReservedInt = (int)bBootSector[37];
+    memcpy(RootCluster, bBootSector + 44, 4);
+    RootClusterInt = (RootCluster[3] << 24) + (RootCluster[2] << 16) + (RootCluster[1] << 8) + RootCluster[0];
+    
+    memcpy(FSInfo, bBootSector + 48, 2);
+    FSInfoInt = (FSInfo[1] << 8) + FSInfo[0];
 
-    BootSig = bBootSector[38];
-    memcpy(VolumeID, bBootSector + 39, 4);
-    memcpy(VolumeLabel, bBootSector + 43, 11);
-    memcpy(FileSystemType, bBootSector + 54, 8);
-    memcpy(BootCode, bBootSector + 62, 448);
+    memcpy(BackupBootSector, bBootSector + 50, 2);
+    BackupBootSectorInt = (BackupBootSector[1] << 8) + BackupBootSector[0];
+
+    memcpy(Reserved, bBootSector + 52, 12);
+
+    PhysicalDriveNumber = bBootSector[64];
+    PhysicalDriveNumberInt = bBootSector[64];
+
+    Reserved2 = bBootSector[65];
+    ExtendedBootSignature = bBootSector[66];
+
+    memcpy(VolumeSerialNumber, bBootSector + 67, 4);
+    memcpy(VolumeLabel, bBootSector + 71, 11);
+    memcpy(FileSystemType, bBootSector + 82, 8);
+    memcpy(BootCode, bBootSector + 90, 420);
+    memcpy(BootSig, bBootSector + 510, 2);
 }
 
-//Display boot sector in decimal
+// Display boot sector in decimal
 void BootSector::DisplayBootSector(){
     cout << "Bytes per sector: " << BytesPerSectorInt << endl;
     cout << "Sectors per cluster: " << SectorsPerClusterInt << endl;
     cout << "Sector before FAT: " << SectorBeforeFatInt << endl;
     cout << "Number of FATs: " << NumberOfFATsInt << endl;
-    cout << "Root entries: " << RootEntriesInt << endl;
-    cout << "Total sectors (if FAT16): " << TotalSectors16Int << endl;
-    cout << "Total sectors (if FAT32): " << TotalSectors32Int << endl;
-    cout << "Sector per track: " << SectorsPerTrackInt << endl;
+    cout << "Sectors per track: " << SectorsPerTrackInt << endl;
     cout << "Number of heads: " << NumberOfHeadsInt << endl;
-    cout << "Hidden sectors: " << HiddenSectorsInt << endl;
+    cout << "Total sectors (if FAT32): " << TotalSectorsInt << endl;
+    cout << "Sectors per FAT: " << SectorsPerFATInt << endl;
+    cout << "Root cluster: " << RootClusterInt << endl;
+    cout << "FSInfo: " << FSInfoInt << endl;
+    cout << "Backup boot sector: " << BackupBootSectorInt << endl;
     cout << "Physical drive number: " << PhysicalDriveNumberInt << endl;    
 }
--- BootSector.h
+++ BootSector.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <Windows.h>
+#include <vector>
 
 using namespace std;
 
@@ -10,6 +11,7 @@ class BootSector{
         ~BootSector();
         void ReadBootSector();
         void DisplayBootSector();
+        void ReadFAT();
     private:
         HANDLE hDevice;
         DWORD dwBytesRead;
@@ -25,30 +27,39 @@ class BootSector{
         BYTE RootEntries[2];
         BYTE TotalSectors16[2];
         BYTE MediaDescriptor;
-        BYTE SectorsPerFAT[2];
+        BYTE SectorsPerFAT16[2];
         BYTE SectorsPerTrack[2];
         BYTE NumberOfHeads[2];
-        BYTE HiddenSectors[4];
+        BYTE Distance[4];
         BYTE TotalSectors32[4];
+        BYTE SectorsPerFAT32[4];
+        BYTE ExtFlag[2];
+        BYTE FSVersion[2]; 
+        BYTE RootCluster[4]; //Cluster bat dau RDET
+        BYTE FSInfo[2]; //Sector chua thong tin phu (cluster trong)
+        BYTE BackupBootSector[2]; //Sector chua ban luu cua boot sector
+        BYTE Reserved[12];
         BYTE PhysicalDriveNumber;
-        BYTE Reserved;
-        BYTE BootSig;
-        BYTE VolumeID[4];
+        BYTE Reserved2;
+        BYTE ExtendedBootSignature;
+        BYTE VolumeSerialNumber[4];
         BYTE VolumeLabel[11];
         BYTE FileSystemType[8];
-        BYTE BootCode[448];
+        BYTE BootCode[420];
+        BYTE BootSig[2];
+
 
         int BytesPerSectorInt;
         int SectorsPerClusterInt;
         int SectorBeforeFatInt;
         int NumberOfFATsInt;
-        int RootEntriesInt;
-        int TotalSectors16Int;
-        int SectorsPerFATInt;
         int SectorsPerTrackInt;
         int NumberOfHeadsInt;
-        int HiddenSectorsInt;
-        int TotalSectors32Int;
+        int TotalSectorsInt;
+        int RootClusterInt;
+        int FSInfoInt;
+        int BackupBootSectorInt;
+        int SectorsPerFATInt;
         int PhysicalDriveNumberInt;
-        int ReservedInt;
+
 };
