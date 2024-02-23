ae1be3888c1b322d12514194f31cbb576848243f
IaTOgN <taingotan0811@gmail.com>
2024-02-23 15:18:57 +0700

    create composite (didnt test)
    composite for file and folder, which will be read from RDET

--- FAT32/BootSector.cpp
+++ FAT32/BootSector.cpp
@@ -170,7 +170,7 @@ vector<uint8_t> BootSector::ReadRDET()
     DWORD bytesRead;
     bool out = false;
     size_t numEntries = 0;
-    while(!out)
+    while (!out)
     {
         if (!ReadFile(hDevice, tmp.data(), 512, &bytesRead, NULL) || bytesRead != 512)
         {
@@ -200,7 +200,7 @@ vector<uint8_t> BootSector::ReadRDET()
         bytesToRead = 512;
     else if (bytesToRead % 512 != 0)
         bytesToRead = (bytesToRead / 512 + 1) * 512;
-    
+
     // Read the RDET entries
     std::vector<uint8_t> Rdet(bytesToRead);
     if (!ReadFile(hDevice, Rdet.data(), bytesToRead, &bytesRead, NULL) || bytesRead != bytesToRead)
@@ -211,12 +211,119 @@ vector<uint8_t> BootSector::ReadRDET()
 
     // Trans rdet to a vector of unsign int for easier calculator
     vector<uint8_t> rdet;
-    for (int i = 0; i < bytesToRead; ++i){
-        if (i % 32 == 0){
+    for (int i = 0; i < bytesToRead; ++i)
+    {
+        if (i % 32 == 0)
+        {
             if (Rdet[i] == 0x00)
                 break;
         }
         rdet.push_back(Rdet[i]);
     }
     return rdet;
+}
+
+// Composite functions
+Item::Item()
+{
+    m_name = "";
+    status = 0;
+    size = 0;
+    StartCluster = 0;
+}
+
+std::string Item::getName()
+{
+    return this->m_name;
+}
+
+File::File() {}
+
+int File::getSize()
+{
+    return this->size;
+}
+
+void File::print(int depth)
+{
+    for (int i = 0; i < depth; i++)
+    {
+        std::cout << "\t";
+    }
+    std::cout << this->m_name << " " << this->size << std::endl;
+}
+
+Folder::Folder() {}
+
+int Folder::getSize()
+{
+    int size = 0;
+    for (int i = 0; i < this->items.size(); i++)
+        size += this->items[i]->getSize();
+
+    return size;
+}
+
+void Folder::print(int depth)
+{
+    for (int i = 0; i < depth; i++)
+    {
+        std::cout << "\t";
+    }
+    std::cout << this->m_name << " " << this->getSize() << std::endl;
+
+    for (int i = 0; i < this->items.size(); i++)
+    {
+        this->items[i]->print(depth + 1);
+    }
+}
+
+void Folder::addItem(Item *item)
+{
+    this->items.push_back(item);
+}
+
+Item *Folder::removeByName(std::string name)
+{
+    for (int i = 0; i < this->items.size(); i++)
+    {
+        if (this->items[i]->getName() == name)
+        {
+            Item *item = this->items[i];
+            this->items.erase(this->items.begin() + i);
+            return item;
+        }
+        else if (Folder *folder = dynamic_cast<Folder *>(this->items[i]))
+        {
+            Item *item = folder->removeByName(name);
+
+            if (item != nullptr)
+            {
+                return item;
+            }
+        }
+    }
+
+    return nullptr;
+}
+Item *Folder::findByName(std::string name)
+{
+    for (int i = 0; i < this->items.size(); i++)
+    {
+        if (this->items[i]->getName() == name)
+        {
+            return this->items[i];
+        }
+        else if (Folder *folder = dynamic_cast<Folder *>(this->items[i]))
+        {
+            Item *item = folder->findByName(name);
+
+            if (item != nullptr)
+            {
+                return item;
+            }
+        }
+    }
+
+    return nullptr;
 }
--- FAT32/BootSector.h
+++ FAT32/BootSector.h
@@ -6,61 +6,94 @@
 #include <stdint.h>
 using namespace std;
 
-class BootSector{
-    public:
-        ~BootSector();
-        void ReadBootSector(string path);
-        void DisplayBootSector();
-        std::vector<uint32_t> ReadFAT();
-        std::vector<uint8_t> ReadRDET();
-    private:
-        HANDLE hDevice;
-        DWORD dwBytesRead;
-        BOOL bResult;
-        BYTE bBootSector[512];
+class BootSector
+{
+public:
+    ~BootSector();
+    void ReadBootSector(string path);
+    void DisplayBootSector();
+    std::vector<uint32_t> ReadFAT();
+    std::vector<uint8_t> ReadRDET();
 
-        BYTE jmpBoot[3];
-        BYTE OEMName[8];
-        BYTE BytesPerSector[2];
-        BYTE SectorsPerCluster;
-        BYTE SectorBeforeFat[2];
-        BYTE NumberOfFATs;
-        BYTE RootEntries[2];
-        BYTE TotalSectors16[2];
-        BYTE MediaDescriptor;
-        BYTE SectorsPerFAT16[2];
-        BYTE SectorsPerTrack[2];
-        BYTE NumberOfHeads[2];
-        BYTE Distance[4];
-        BYTE TotalSectors32[4];
-        BYTE SectorsPerFAT32[4];
-        BYTE ExtFlag[2];
-        BYTE FSVersion[2]; 
-        BYTE RootCluster[4]; //Cluster bat dau RDET
-        BYTE FSInfo[2]; //Sector chua thong tin phu (cluster trong)
-        BYTE BackupBootSector[2]; //Sector chua ban luu cua boot sector
-        BYTE Reserved[12];
-        BYTE PhysicalDriveNumber;
-        BYTE Reserved2;
-        BYTE ExtendedBootSignature;
-        BYTE VolumeSerialNumber[4];
-        BYTE VolumeLabel[11];
-        BYTE FileSystemType[8];
-        BYTE BootCode[420];
-        BYTE BootSig[2];
+private:
+    HANDLE hDevice;
+    DWORD dwBytesRead;
+    BOOL bResult;
+    BYTE bBootSector[512];
 
+    BYTE jmpBoot[3];
+    BYTE OEMName[8];
+    BYTE BytesPerSector[2];
+    BYTE SectorsPerCluster;
+    BYTE SectorBeforeFat[2];
+    BYTE NumberOfFATs;
+    BYTE RootEntries[2];
+    BYTE TotalSectors16[2];
+    BYTE MediaDescriptor;
+    BYTE SectorsPerFAT16[2];
+    BYTE SectorsPerTrack[2];
+    BYTE NumberOfHeads[2];
+    BYTE Distance[4];
+    BYTE TotalSectors32[4];
+    BYTE SectorsPerFAT32[4];
+    BYTE ExtFlag[2];
+    BYTE FSVersion[2];
+    BYTE RootCluster[4];      // Cluster bat dau RDET
+    BYTE FSInfo[2];           // Sector chua thong tin phu (cluster trong)
+    BYTE BackupBootSector[2]; // Sector chua ban luu cua boot sector
+    BYTE Reserved[12];
+    BYTE PhysicalDriveNumber;
+    BYTE Reserved2;
+    BYTE ExtendedBootSignature;
+    BYTE VolumeSerialNumber[4];
+    BYTE VolumeLabel[11];
+    BYTE FileSystemType[8];
+    BYTE BootCode[420];
+    BYTE BootSig[2];
 
-        int BytesPerSectorInt;
-        int SectorsPerClusterInt;
-        int SectorBeforeFatInt;
-        int NumberOfFATsInt;
-        int SectorsPerTrackInt;
-        int NumberOfHeadsInt;
-        int TotalSectorsInt;
-        int RootClusterInt;
-        int FSInfoInt;
-        int BackupBootSectorInt;
-        int SectorsPerFATInt;
-        int PhysicalDriveNumberInt;
+    int BytesPerSectorInt;
+    int SectorsPerClusterInt;
+    int SectorBeforeFatInt;
+    int NumberOfFATsInt;
+    int SectorsPerTrackInt;
+    int NumberOfHeadsInt;
+    int TotalSectorsInt;
+    int RootClusterInt;
+    int FSInfoInt;
+    int BackupBootSectorInt;
+    int SectorsPerFATInt;
+    int PhysicalDriveNumberInt;
+};
 
+class Item
+{
+protected:
+    std::string m_name;
+    int status;
+    int size;
+    int StartCluster;
+public:
+    Item();
+    std::string getName();
+    virtual int getSize() = 0;
+    virtual void print(int depth) = 0;
+};
+
+class File : public Item{
+public:
+    File();
+    int getSize();
+    void print(int depth);
+};
+
+class Folder : public Item{
+private:
+    std::vector<Item*> items;
+public:
+    Folder();
+    int getSize();
+    void print(int depth);
+    void addItem(Item* item);
+    Item* removeByName(std::string name);
+    Item* findByName(std::string name);
 };
