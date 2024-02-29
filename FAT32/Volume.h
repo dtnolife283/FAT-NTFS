68bc9e5cf1d96199230c8192698005d2537c4ae6
joecao0303 <joecao0303@gmail.com>
2024-02-29 22:37:01 +0700

    correct errror
    correct transRdet and read cluster from entry

--- FAT32/Volume.h
+++ FAT32/Volume.h
@@ -8,7 +8,7 @@ struct Volume{
     std::vector<uint32_t> fatData;
     std::vector<uint8_t> RDETData;
 
-    vector<Item*> TransRdet(vector<uint8_t>& data, bool flag, uint32_t startOffset);
+    vector<Item*> TransRdet(vector<uint8_t>& data, bool flag, uint32_t startOffset, uint32_t& startCluster);
     vector<uint8_t> ReadSector(int sector , int numSector);
     void read_fatData();
     void read_RDETData();
